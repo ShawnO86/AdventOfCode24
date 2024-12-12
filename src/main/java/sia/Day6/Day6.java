08583822b6cd54357d71a053a73707b0fbc32ebe
Shawn Osborne <shawnby222@gmail.com>
2024-12-12 14:26:24 -0600

    Solves day 6 part 1

--- src/main/java/sia/Day6/Day6.java
+++ src/main/java/sia/Day6/Day6.java
@@ -2,24 +2,28 @@ package sia.Day6;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day6 {
     private final File inputFile;
-    private final char[][] board = new char[130][130];
-    //up, right, down, left
-    private final int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    private int[] currDirection = directions[0];
+    public final char[][] board = new char[130][130];
 
-    public int visited = 0;
+    //up, right, down, left (x, y)
+    private final List<int[]> directions = new ArrayList<>(List.of(new int[]{-1, 0}, new int[]{0, 1}, new int[]{1, 0}, new int[]{0, -1}));
+    private int[] currDirection = directions.getFirst();
+    private final int[] currPos;
+    private boolean isInArea = true;
+
+    public Set<List<Integer>> visitedPositions = new HashSet<List<Integer>>();
 
     public Day6(File inputFile) throws FileNotFoundException {
         this.inputFile = inputFile;
         readInput();
+        this.currPos = getStartingPoint();
     }
 
     private void readInput() throws FileNotFoundException {
-        Scanner sc = new Scanner(this.inputFile);
+        Scanner sc = new Scanner(inputFile);
         int index = 0;
         while (sc.hasNextLine()) {
             char[] line = sc.nextLine().toCharArray();
@@ -29,17 +33,61 @@ public class Day6 {
         sc.close();
     }
 
-    private void setNextDirection() {
-        if (currDirection == directions[directions.length - 1]) {
-            currDirection = directions[0];
-        } else {
-            for (int i = 0; i < directions.length; i++) {
-                if (currDirection == directions[i]) {
-                    currDirection = directions[i + 1];
+    private int[] getStartingPoint() {
+        int[] startingPoint = {-1, -1};
+        for (int i = 0; i < 130; i++) {
+            for (int j = 0; j < 130; j++) {
+                if (board[i][j] == '^') {
+                    startingPoint[0] = i;
+                    startingPoint[1] = j;
                 }
             }
         }
+        visitedPositions.add(List.of(startingPoint[0], startingPoint[1]));
+        return startingPoint;
     }
 
+    private void moveForward() {
+        int newX = currPos[0] + currDirection[0];
+        int newY = currPos[1] + currDirection[1];
+        System.out.println(visitedPositions);
+        //in area bounds check
+        if (newX < 0 || newX >= board.length || newY < 0 || newY >= board[currPos[0]].length) {
+            //guard is leaving area
+            isInArea = false;
+            return;
+        }
+        System.out.println("Current pos: " + currPos[0] + ", " + currPos[1] + " new pos: " + newX + ", " + newY);
+        System.out.println("Char: " + board[newX][newY]);
+        //check for '#' to signify an obstruction then setNextDirection
+        if (board[newX][newY] == '#') {
+            System.out.println("Turning . . . ");
+            setNextDirection();
+            return;
+        }
 
+        //If no direction change - move to new position and increment visited position
+        //Visited positions are not counted twice - using Set.
+        visitedPositions.add(List.of(newX, newY));
+        currPos[0] = newX;
+        currPos[1] = newY;
+    }
+
+    //guard turns right 90 degrees
+    private void setNextDirection() {
+        System.out.println("Current direction: " + Arrays.toString(currDirection));
+        if (currDirection[0] == directions.getLast()[0] && currDirection[1] == directions.getLast()[1]) {
+            currDirection = directions.getFirst();
+        } else {
+            int currDirectionIndex = directions.indexOf(currDirection) + 1;
+            currDirection = directions.get(currDirectionIndex);
+        }
+        System.out.println("Next direction: " + Arrays.toString(currDirection));
+    }
+
+    public void simulatePatrol() {
+        while (isInArea) {
+            moveForward();
+        }
+    }
 }
--- src/main/java/sia/Day6/Day6Runner.java
+++ src/main/java/sia/Day6/Day6Runner.java
@@ -2,10 +2,13 @@ package sia.Day6;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Day6Runner {
     public static void main(String[] args) throws FileNotFoundException {
         File inputFile = new File("D:\\adventOfCode24\\Advent_of_code_24\\src\\main\\java\\sia\\Day6\\input.txt");
         Day6 day6 = new Day6(inputFile);
+        day6.simulatePatrol();
+        System.out.println(day6.visitedPositions.size());
     }
 }
